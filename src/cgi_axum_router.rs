5017a88bb5ad8f013386e82ca65af380b758e70f
Marcel Koch <kontakt@marcelkoch.net>
2022-09-18 19:09:45 +0200

    implement the first cgi_axum_router

---
+++ src/cgi_axum_router.rs
@@ -0,0 +1,63 @@
+use axum::handler::Handler;
+use axum::http::request::Parts;
+use axum::http::Request;
+use axum::Router;
+use cgi::http::HeaderValue;
+use hyper::header::CONTENT_TYPE;
+use hyper::Body;
+use tower_service::Service;
+
+pub fn handle(mut router: Router) {
+    cgi::handle(|request: cgi::Request| -> cgi::Response {
+        let future = async {
+            let response = router.call(convert_request(request)).await.unwrap();
+            let parts = response.into_parts();
+            let content_type = match parts.0.headers.get(CONTENT_TYPE) {
+                Some(x) => Option::Some(x.to_str().unwrap()),
+                None => Option::None,
+            };
+            let bytes = hyper::body::to_bytes(parts.1).await.unwrap();
+
+            cgi::binary_response(parts.0.status, content_type, bytes.into())
+        };
+
+        futures::executor::block_on(future)
+    });
+}
+
+fn convert_request(request: cgi::Request) -> axum::http::Request<Body> {
+    let parts = request.into_parts();
+
+    let real_parts = parts.0;
+
+    let mut builder = Request::builder();
+
+    builder.headers_mut().unwrap().extend(real_parts.headers);
+
+    let axum_request = builder
+        .method(real_parts.method)
+        .uri(std::env::var("PATH_INFO").unwrap())
+        .body(parts.1.into());
+
+    axum_request.unwrap()
+}
+
+mod tests {
+    use super::*;
+    use axum::routing::get;
+
+    async fn root() -> String {
+        "Hello World".to_string()
+    }
+
+    #[test]
+    fn test_handle() {
+        std::env::set_var("REQUEST_METHOD", "GET");
+        std::env::set_var("SCRIPT_NAME", "axum.cgi");
+        std::env::set_var("PATH_INFO", "/bla");
+
+        let router = Router::new().route("/", get(root));
+
+        handle(router)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod cgi_axum_router;
+
+use axum::handler::Handler;
+use axum::response::Response;
+use axum::{routing::get, Router};
+
+fn main() {}
